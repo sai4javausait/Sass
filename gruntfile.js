fc8a019e0418485110f7dd484d9a5b9dd49c2f0e
Sai Java <sai4javausait@gmail.com>
2018-04-14 16:43:10 -0500

    Installed all Dependencies for Sass and Configured Grunt.js

---
+++ gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        sass: {                              // Task 
+            dist: {                            // Target job
+              options: {                       // Target options 
+                style: 'expanded'
+              },
+              files: [{                         // Dictionary of files 
+                src: 'process/sass/style.scss',
+                dest: 'builds/sassEssentials/css/style.css'
+              }]
+            }
+          }, // sass
+
+        watch: {
+            scripts: {
+                files: ['builds/sassEssentials/**/*.html',
+                        'process/sass/**/*.scss'],
+                tasks: ['sass']
+            },
+            options: {
+                spawn: false,
+                livereload: true
+            }
+        },//watch
+
+        connect: {
+            server: {
+                options: {
+                  port: 8000,
+                  hostname: 'localhost',
+                  base: 'builds/sassEssentials',
+                  livereload: true
+                }
+              }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.registerTask('default', ['sass', 'connect', 'watch']);
+}; //module
